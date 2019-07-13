6affd61ee76326e767e34d971a721b9b94dee700
Άρης Παντελάκης <aris-pant@hotmail.com>
2019-07-13 12:32:20 +0300

    Set a button for geting current location using mapbox

--- public/js/app.js
+++ public/js/app.js
@@ -4,9 +4,30 @@ const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
 
+
+document.querySelector('#get-location').addEventListener('click', (e) => {
+    e.preventDefault()
+    if(!navigator.geolocation){
+        messageOne = 'This feature is not supported by your browser!'
+    }
+    
+    navigator.geolocation.getCurrentPosition((position) => {
+      var url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+position.coords.longitude+','+position.coords.latitude+'.json?access_token=pk.eyJ1IjoiYXJpc3BhbnQiLCJhIjoiY2p2cDU1bG5zMjV3dDRhb2k4N2VxcnBoYiJ9.9apBzez3gvOpJh6pjk9DYw&limit=1';
+      fetch(url)
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+            search.value = data.features[0].properties.address
+        }) 
+    })
+})
+
+
 weatherForm.addEventListener('submit', (e) =>{
     e.preventDefault()
-
+    
     const location = search.value
 
     messageOne.textContent = 'Loading...'
@@ -15,12 +36,11 @@ weatherForm.addEventListener('submit', (e) =>{
     fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) =>{
             if (data.error) {
-                messageOne.textContent = data.error
+              messageOne.textContent = data.error
             } else {
-                messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
-            }
-            
+              messageOne.textContent = data.location
+              messageTwo.textContent = data.forecast
+            }  
         })
     })
 })
